c83ec9c3cac19aa6f89b3a8c9a263eb78ddc7068
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-22 15:17:36 +0100

    more authorization

--- frontend/src/component/AppLayout.js
+++ frontend/src/component/AppLayout.js
@@ -4,14 +4,22 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../containers/auth/Login';
 import Register from '../containers/auth/Register';
 import NavBar from './NavBar';
+import Home from '../containers/Home';
+
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../store/selectors/AuthSelectors';
 
 const AppLayout = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
+        {isLoggedIn && <Route path="*" element={<Home />} />}
+        {!isLoggedIn && <Route path="*" element={<Login />} />}
+        {!isLoggedIn && <Route exact path="/register" element={<Register />} />}
+        {!isLoggedIn && <Route exact path="/login" element={<Login />} />}
       </Routes>
     </div>
   );
