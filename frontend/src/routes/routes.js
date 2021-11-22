9df97b1699742f0209423a37e2c252a354bc1ec6
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-22 18:34:10 +0100

    const routes

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,8 @@
+export const LOGIN = '/login';
+export const LOGOUT = '/register';
+export const REGISTER = '/logout';
+
+export const HOME = '/home';
+export const ABOUT = '/about';
+
+export const ANY = '*';
