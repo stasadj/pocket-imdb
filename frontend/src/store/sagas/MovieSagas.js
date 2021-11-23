c1b817caa63f5f14d815c87d0ed7d05d198b4487
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-23 17:35:58 +0100

    pagination backend

--- frontend/src/store/sagas/MovieSagas.js
+++ frontend/src/store/sagas/MovieSagas.js
@@ -3,9 +3,9 @@ import { call, put } from 'redux-saga/effects';
 import { movieService } from '../../services/MovieService';
 import { setMovies } from '../actions/MovieActions';
 
-export function* moviesGet() {
+export function* moviesGet({ payload }) {
   try {
-    const { data } = yield call(movieService.getMovies);
+    const { data } = yield call(movieService.getMovies, payload);
 
     yield put(setMovies(data));
   } catch (error) {
