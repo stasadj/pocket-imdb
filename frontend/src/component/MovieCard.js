3eddb8226529191d23b568cfb0f6ca8cc8c858ce
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-23 12:55:46 +0100

    movie list frontend

--- frontend/src/component/MovieCard.js
+++ frontend/src/component/MovieCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import Card from 'react-bootstrap/Card';
+
 const MovieCard = ({ movie }) => {
   return (
-    <div>
-      <p>{movie.title}</p>
-    </div>
+    <Card className="mb-1" style={{ maxWidth: '33%' }}>
+      <Card.Img variant="top" src={movie.cover} />
+      <Card.Body>
+        <Card.Title>{movie.title} </Card.Title>
+        <Card.Text>{movie.description}</Card.Text>
+      </Card.Body>
+      <Card.Footer style={{ color: 'white' }}>
+        <small className="text-muted">{movie.genre}</small>
+      </Card.Footer>
+    </Card>
   );
 };
 
