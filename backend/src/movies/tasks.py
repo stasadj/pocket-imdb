cca1e5578a7686b67e905507acdaafa7967f952c
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-30 15:12:41 +0100

    added celery

---
+++ backend/src/movies/tasks.py
@@ -0,0 +1,15 @@
+from django.core.mail import send_mail
+from django.core.exceptions import BadRequest
+from celery import shared_task
+
+
+@shared_task(autoretry_for=(TimeoutError, BadRequest,))
+def send_mail_to_admin(movie):
+    send_mail(
+        'New movie: {}'.format(movie['title']),
+        'Title: {}\nDescription: {}\nGenre: {}'.format(
+            movie['title'], movie['description'], movie['genre']),
+        'from@example.com',
+        ['to@example.com'],
+        fail_silently=False,
+    )
