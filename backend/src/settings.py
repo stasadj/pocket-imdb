4a1b3b1ab52f2bf3d3122aeb8aafcc74b31b3877
stasadj <anastasijadjuric@uns.ac.rs>
2021-11-30 12:51:35 +0100

    file upload backend

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -188,3 +189,11 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_URL = 'static/'
